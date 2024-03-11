8b332a2d139df0848440fe1ba60e3c1f9ee1a12d
lxfu1 <954055752@qq.com>
2024-03-11 14:56:18 +0800

    feat: init osgraph web

---
+++ osgraph-web/src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ osgraph-web/src/utils/index.ts
@@ -0,0 +1,1 @@
+export { LocalStore } from "./local-store";
