6413ad29ae635f1902b2758239e0c4cc92de03ec
yuanxiaohua.yxh <yuanxiaohua.yxh@alibaba-inc.com>
2024-04-23 19:00:47 +0800

    feat: modify build and start script

--- bin/build-osgraph.sh
+++ bin/build-osgraph.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "pre install maven >= 3.6.3"
 echo "pre install npm"
-#npm run build 和 dist
+
 echo "build osgraph-service"
 mkdir -p ~/.m2/repository/com/alipay/tumaker/
 cp -r osgraph-service/libs/tumaker-serviceplugin-devops ~/.m2/repository/com/alipay/tumaker/
@@ -10,9 +10,11 @@ cd osgraph-service
 mvn clean install -Dmaven.test.skip=true
 cp -r target ../
 cd ../
+
 echo "build osgraph-web"
 cd osgraph-web
 npm run build
 mv dist ../
 cd ../
-echo "start service, default port is 9000"
+
+
--- osgraph-service/conf/start-osgraph.sh
+++ osgraph-service/conf/start-osgraph.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
-echo "pre install maven >= 3.6.3"
-echo "pre install npm"
-#npm run build 和 dist
-echo "build osgraph-service"
-mkdir -p ~/.m2/repository/com/alipay/tumaker/
-cp -r osgraph-service/libs/tumaker-serviceplugin-devops ~/.m2/repository/com/alipay/tumaker/
-cp -r osgraph-service/libs/tumakertugraphdb-parent ~/.m2/repository/com/alipay/tumaker/
-cd osgraph-service
-mvn clean install -Dmaven.test.skip=true
-cp -r target ../
-cd ../
-echo "build osgraph-web"
-cd osgraph-web
-npm run build
-mv dist ../
-cd ../
 echo "please set mysql, es, tugraphdb in target/config/application.properties"
-echo "start service, default port is 9000"
+echo "start service, default port is 8888, nginx port is 80"
 
 #start nginx
 dir=`pwd`
@@ -28,5 +12,3 @@ mkdir -p tumaker-bootstrap-1.0.1-executable
 mv BOOT-INF META-INF org tumaker-bootstrap-1.0.1-executable/
 cd ${dir}
 java -Xms3800m -Xmx3800m -Xmn1500m -Xss512k -XX:PermSize=512m -XX:MaxPermSize=512m   -Dzmode=false -Dspring.config.additional-location=${dir}/target/config/ -Dspring.profiles.active=prod -classpath ${dir}/target/boot/tumaker-bootstrap-1.0.1-executable org.springframework.boot.loader.JarLauncher
-
-
