0b94cba76050a1d2a60e5439fe1361f7e8a5e970
Sean <37206679+Seventysevendays@users.noreply.github.com>
2024-04-28 17:31:39 +0800

    feat: optimize home style (#34)

--- osgraph-web/src/result/index.tsx
+++ osgraph-web/src/result/index.tsx
@@ -68,7 +68,7 @@ export default () => {
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
     a.href = url;
-    a.download = searchValue || "os graph";
+    a.download = warehouseValue || "os graph";
     a.click();
   };
   const getGraphLoading = (loading: boolean) => {
