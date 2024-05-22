77e8c71ce122cb373f03f3fd99aaa652761101d4
yuanxiaohua.yxh <yuanxiaohua.yxh@alibaba-inc.com>
2024-05-22 15:58:06 +0800

    feat:clean abandoned dependent jar

--- bin/build.sh
+++ bin/build.sh
@@ -6,12 +6,10 @@ set -x
 workdir=`pwd`
 echo "build osgraph-service"
 mkdir -p ~/.m2/repository/com/alipay/tumaker/
-cp -r osgraph-service/libs/tumaker-serviceplugin-devops ~/.m2/repository/com/alipay/tumaker/
 cp -r osgraph-service/libs/tumaker-serviceplugin-core ~/.m2/repository/com/alipay/tumaker/
 cp -r osgraph-service/libs/tumakertugraphdb-parent ~/.m2/repository/com/alipay/tumaker/
 #cp -r osgraph-service/libs/tumakertugraphdb-core ~/.m2/repository/com/alipay/tumaker/
 cd osgraph-service
-mvn install:install-file -DgroupId=com.alipay.tumaker -DartifactId=tumaker-serviceplugin-devops -Dversion=1.0.2 -Dpackaging=jar -Dfile=./libs/tumaker-serviceplugin-devops/1.0.2/tumaker-serviceplugin-devops-1.0.2.jar
 mvn install:install-file -DgroupId=com.alipay.tumaker -DartifactId=tumaker-serviceplugin-core -Dversion=1.0.1 -Dpackaging=jar -Dfile=./libs/tumaker-serviceplugin-core/1.0.1/tumaker-serviceplugin-core-1.0.1.jar
 mvn install:install-file -DgroupId=com.alipay.tumaker -DartifactId=tumakertugraphdb-core -Dversion=1.0.1-SNAPSHOT -Dpackaging=jar -Dfile=./libs/tumakertugraphdb-core/1.0.1-SNAPSHOT/tumakertugraphdb-core-1.0.1-SNAPSHOT.jar
 mvn clean install -Dmaven.test.skip=true
