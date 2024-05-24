266c325d2e9fb2fe00da6876d0f1495e4ca27291
xiaohua2014 <yuanxiaohua2013@gmail.com>
2024-05-24 20:41:09 +0800

    fet: add serviceplugin version

--- bin/build-osgraph.sh
+++ bin/build-osgraph.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 echo "please set mysql, es, tugraphdb in env"
 echo "start service, default port is 8888, nginx port is 80"
-set -x
-#!/bin/bash
 workdir=`pwd`
 echo "build osgraph-service"
 mkdir -p ~/.m2/repository/com/alipay/tumaker/
+rm -rf ~/.m2/repository/com/alipay/tumaker/*
 cp -r osgraph-service/libs/tumaker-serviceplugin-core ~/.m2/repository/com/alipay/tumaker/
 cp -r osgraph-service/libs/tumakertugraphdb-parent ~/.m2/repository/com/alipay/tumaker/
-#cp -r osgraph-service/libs/tumakertugraphdb-core ~/.m2/repository/com/alipay/tumaker/
+cp -r osgraph-service/libs/tumakertugraphdb-core ~/.m2/repository/com/alipay/tumaker/
 cd osgraph-service
-mvn install:install-file -DgroupId=com.alipay.tumaker -DartifactId=tumakertugraphdb-core -Dversion=1.0.1-SNAPSHOT -Dpackaging=jar -Dfile=./libs/tumakertugraphdb-core/1.0.1-SNAPSHOT/tumakertugraphdb-core-1.0.1-SNAPSHOT.jar
 mvn clean install -Dmaven.test.skip=true
 cp -r target /home/admin/release/run/
 mkdir -p /home/admin/release/run/target/boot/tumaker-bootstrap-1.0.1-executable
