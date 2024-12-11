93ccf81d52fe88c5485c836d2c020e7f6403c519
liukaiming-alipay <lkm01250564@antgroup.com>
2024-12-11 10:55:00 +0800

    feat: zh and us (#82)
    * feat: zh and us
    
    * feat： test code

--- osgraph-web/src/404/index.tsx
+++ osgraph-web/src/404/index.tsx
@@ -1,21 +1,32 @@
 /** @jsxImportSource @emotion/react */
 import { useTranslation } from "react-i18next";
-import { Button, Result } from 'antd';
+import { Button, Result } from "antd";
 import { useNavigate } from "react-router-dom";
 
-const PageNotFound: React.FC<{source?: string}> = props => {
+const PageNotFound: React.FC<{ source?: string }> = (props) => {
   const { t } = useTranslation();
   const navigate = useNavigate();
-  const { source = '404' } = props;
+  const { source = "404" } = props;
   return (
-      <div style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-        <Result
-          status="404"
-          title={t(source === '404' ? '页面未找到' : '参数错误')}
-          subTitle={t(source === '404' ? 'Page Not Found' : 'Parameter Error')}
-          extra={<Button type="primary" onClick={() => navigate('/')}>{t('回到主页')}</Button>}
-        />
-      </div>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Result
+        status="404"
+        title={t(source === "404" ? "页面未找到" : "参数错误")}
+        subTitle={t(source === "404" ? "Page Not Found" : "Parameter Error")}
+        extra={
+          <Button type="primary" onClick={() => navigate("/")}>
+            {t("goHome")}
+          </Button>
+        }
+      />
+    </div>
   );
 };
 
