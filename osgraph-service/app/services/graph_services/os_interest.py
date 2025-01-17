7d332fcd6755073701d680f4735b02624e3661ae
KingSkyLi <15566300566@163.com>
2025-01-17 13:12:42 +0800

    add file header;

--- osgraph-service/app/services/graph_services/os_interest.py
+++ osgraph-service/app/services/graph_services/os_interest.py
@@ -50,7 +50,20 @@ class OSInterestService(BaseService):
         res = es.search(index=f"{platform}_{input}", query=query, size=1)
         if len(res):
             user_id = res[0]["id"]
-            params_dict = {
+            params_dict = {#
+# Copyright 2025 AntGroup CO., Ltd.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+
                 "developer_id": user_id,
                 "topic_topn": topic_limit,
                 "repo_topn": repo_limit,
--- osgraph-service/server.py
+++ osgraph-service/server.py
@@ -1,16 +1,17 @@
-'''
-/*
- * @Author: KingSkyLi (15566300566@163.com)
- * @File Created: 2025-01-17
- * @Modified By: KingSkyLi (15566300566@163.com)
- * @Last Modified: 2025-01-17
- * @Description: 
- * @Copyright: 2023 - 2025 TuGraph, Ant Group
- */
-'''
+#
+# Copyright 2025 AntGroup CO., Ltd.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
 
-
-# server.py
 import os
 
 import flask
