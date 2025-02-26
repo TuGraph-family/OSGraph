443ab5f526ae53769bbfcc596e22e6260d0c7e9f
liukaiming-alipay <lkm01250564@antgroup.com>
2025-02-26 11:25:06 +0800

    feat: standardization

--- osgraph-web/src/utils/date.ts
+++ osgraph-web/src/utils/date.ts
@@ -3,8 +3,8 @@
  * author: Allen
  */
 
-const timestampToDate = (timestamp: number): string => {
-  const date = new Date(timestamp * 1000);
+const timestampToDate = (timestamp: any): string => {
+  const date = new Date(timestamp);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
