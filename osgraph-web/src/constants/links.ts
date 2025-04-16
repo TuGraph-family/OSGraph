bd3557a4b2df73137d4f143aace587b34f29158c
liukaiming-alipay <lkm01250564@antgroup.com>
2025-04-16 18:12:47 +0800

    feat(web): Plugin and style optimization

--- osgraph-web/src/constants/links.ts
+++ osgraph-web/src/constants/links.ts
@@ -1,6 +1,28 @@
-export const X_LAB_GITHUB = "https://github.com/X-lab2017?language=shell";
-export const TUGRAPH_TECH = "https://www.tugraph.tech/";
-export const ANTV = "https://antv.antgroup.com/";
-export const OSGRAPH_GITHUB = "https://github.com/TuGraph-family/OSGraph";
-export const DOWNLOAD_ICON = 'https://mdn.alipayobjects.com/huamei_tu4rvn/afts/img/A*2tgHRoQUeIYAAAAAAAAAAAAAep_eAQ/original'
-export const SHARE_ICON = 'https://mdn.alipayobjects.com/huamei_tu4rvn/afts/img/A*UM3WSJM4MH4AAAAAAAAAAAAAep_eAQ/original'
+const X_LAB_GITHUB = "https://github.com/X-lab2017?language=shell";
+const TUGRAPH_TECH = "https://www.tugraph.tech/";
+const ANTV = "https://antv.antgroup.com/";
+const OSGRAPH_GITHUB = "https://github.com/TuGraph-family/OSGraph";
+const DOWNLOAD_ICON = 'https://mdn.alipayobjects.com/huamei_tu4rvn/afts/img/A*2tgHRoQUeIYAAAAAAAAAAAAAep_eAQ/original'
+const SHARE_ICON = 'https://mdn.alipayobjects.com/huamei_tu4rvn/afts/img/A*UM3WSJM4MH4AAAAAAAAAAAAAep_eAQ/original'
+const CHROME_ICON = 'https://mdn.alipayobjects.com/huamei_tu4rvn/afts/img/A*mIanS4WXt9QAAAAAAAAAAAAAep_eAQ/original'
+const EDGE_ICON = 'https://mdn.alipayobjects.com/huamei_tu4rvn/afts/img/A*RKIHRraIL7AAAAAAAAAAAAAAep_eAQ/original';
+const PLUGIN_ICON = 'https://mdn.alipayobjects.com/huamei_tu4rvn/afts/img/A*aYnUS5yHhO8AAAAAAAAAAAAAep_eAQ/original'
+
+const PLUGIN_CHROME = 'https://chrome.google.com/webstore/detail/hypercrx/ijchfbpdgeljmhnhokmekkecpbdkgabc';
+const PlUGIN_EDGE = 'https://microsoftedge.microsoft.com/addons/detail/hypercrx/lbbajaehiibofpconjgdjonmkidpcome';
+const PLUGIN_GITHUB = 'https://github.com/hypertrons/hypertrons-crx'
+
+export {
+    X_LAB_GITHUB,
+    TUGRAPH_TECH,
+    ANTV,
+    OSGRAPH_GITHUB,
+    DOWNLOAD_ICON,
+    SHARE_ICON,
+    CHROME_ICON,
+    EDGE_ICON,
+    PLUGIN_ICON,
+    PLUGIN_CHROME,
+    PlUGIN_EDGE,
+    PLUGIN_GITHUB
+}
